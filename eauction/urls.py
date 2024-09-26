8cd600cef35ab9a22049ff2cdfc29e3ca675e92f
Ayub Shrestha <ayubsht1@gmail.com>
2024-09-26 15:52:52 +0545

    basic urls and views

--- eauction/urls.py
+++ eauction/urls.py
@@ -1,7 +1,12 @@
 from django.contrib import admin
 from django.urls import path
-from .views import HomeView
+from .views import HomeView, AuctionView, SellItemsView, WatchListView,CategoryView
 
 urlpatterns = [
     path('', HomeView.as_view(), name='home'),
+    path('auction-list/', AuctionView.as_view(), name='auction-list'),
+    path('sell-items/', SellItemsView.as_view(), name='sell-items'),
+    path('watch-list/', WatchListView.as_view(), name='watch-list'),
+    path('category/', CategoryView.as_view(), name='category'),
+    
 ]
--- eauction/views.py
+++ eauction/views.py
@@ -5,3 +5,16 @@ from django.views.generic import TemplateView
 # Create your views here.
 class HomeView(TemplateView):
     template_name = 'eauction/home.html'
+
+class AuctionView(TemplateView):
+    template_name = 'eauction/auctions/auction_list.html'
+
+class SellItemsView(TemplateView):
+    template_name = 'eauction/seller/sell_items.html'
+
+class WatchListView(TemplateView):
+    template_name = 'eauction/watchlist/watch_list.html'
+
+class CategoryView(TemplateView):
+    template_name = 'eauction/category/category.html'
+
